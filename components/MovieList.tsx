2731ca2530927c13941ac49f1d34abda7c30fa24
roabhi <teskostudio@gmail.com>
2023-04-12 15:00:28 +0200

    Added responsive design

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -13,12 +13,12 @@ const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
   }
 
   return (
-    <div className="px-4 md:px-12 mt-4 space-y-8">
+    <div className="mt-[15%] px-4 md:px-12 md:mt-4 space-y-8">
       <div>
         <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
           {title}
         </p>
-        <div className="grid grid-cols-4 gap-2">
+        <div className="flex flex-col mb-4 md:grid grid-cols-2 gap-2 xl:grid-cols-4">
           {data.map((movie) => (
             <MovieCard key={movie.id} data={movie} />
           ))}
